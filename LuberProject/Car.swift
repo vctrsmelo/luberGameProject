8ecb5b8c7f98177303bc0fa9e798d2719411d559
Victor S Melo <victor.contas@outlook.com>
2017-06-22 14:49:22 -0300

    update car and luber classes

--- LuberProject/Car.swift
+++ LuberProject/Car.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import SpriteKit
+
+enum Lane {
+    case 1,
+    case 2,
+    case 3
+}
+
+class Car {
+    
+    var spriteNode: SKSpriteNode
+    var currentLane: Lane
+    
+    init(spriteName: String, currentLane: Lane) {
+        self.spriteNode = SKSpriteNode(fileNamed: spriteName)
+        self.currentLane = currentLane
+    }
+}
