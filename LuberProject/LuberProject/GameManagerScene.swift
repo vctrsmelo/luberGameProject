5aa6d96a474d5e5eb8980a9b3f088c1045fae1c9
Cristiano Salla Lunardi <cristianosalla@gmail.com>
2017-06-23 14:28:22 -0300

    merge

--- LuberProject/LuberProject/GameManagerScene.swift
+++ LuberProject/LuberProject/GameManagerScene.swift
@@ -10,28 +10,31 @@ import SpriteKit
 import GameplayKit
 
 class GameManagerScene: SKScene {
-	
-	private var label : SKLabelNode?
-	private var spinnyNode : SKShapeNode?
-	var taxis: [Taxi] = []
 	var luber: Luber!
+	var taxis: [Taxi] = []
 	
 	private var lane1: SKNode!
 	private var lane2: SKNode!
 	private var lane3: SKNode!
 	
-	private var kmLabel : SKLabelNode!
-	private var highscoreLabel : SKLabelNode!
-	
-	
 	private let TAXI_SPRITE_NAME: String = "Car01_test02"
 	
-	
-	//variavel temporaria
-	private var endGame : Int!
+	private var endGame : Int?
+	private var highscoreLabel : SKLabelNode?
 	
 	
 	override func sceneDidLoad() {
+		if let lane1 = self.childNode(withName: "lane1"), let lane2 = self.childNode(withName: "lane2"), let lane3 = self.childNode(withName: "lane3"){
+			self.lane1 = lane1
+			self.lane2 = lane2
+			self.lane3 = lane3
+		}
+	}
+	
+	override func didMove(to view: SKView) {
+		luber = Luber(spriteName: "Car01_test02", currentLane: 2)
+		luber.addPlayerSwipeRecognizer(to: self.view!)
+		addChild(luber.spriteNode)
 		
 		if let lane1 = self.childNode(withName: "lane1"), let lane2 = self.childNode(withName: "lane2"), let lane3 = self.childNode(withName: "//lane3"){
 			self.lane1 = lane1
@@ -40,18 +43,8 @@ class GameManagerScene: SKScene {
 		}
 		//addTaxi(atLane: 3, carYDistance: 0, taxiSpeed: 1)
 		
-	}
-	
-	override func didMove(to view: SKView) {
-		
 		endGame = Int(arc4random_uniform(2) + 1)
 		
-		
-		
-		luber = Luber(spriteName: "Car01_test02", currentLane: 2)
-		
-		luber.addPlayerSwipeRecognizer(to: self.view!)
-		addChild(luber.spriteNode)
 		
 		Background.shared.background = self.childNode(withName: "background") as? SKSpriteNode
 		Background.shared.background2 = self.childNode(withName: "background2") as? SKSpriteNode
@@ -59,22 +52,62 @@ class GameManagerScene: SKScene {
 		Background.shared.scene = self
 		Background.shared.speed = -15
 		
-		self.highscoreLabel = self.childNode(withName: "highscore") as! SKLabelNode
-		
+		// TAXI TEST
+		addTaxi(atLane: 3, carYDistance: 0, taxiSpeed: 1)
+		let moveTaxi = SKAction.move(by: CGVector(dx: 0, dy: -1500), duration: 5)
+		let taxi: Taxi = taxis[0]
+		taxi.spriteNode.run(moveTaxi, withKey: "taxiTest")
+		// END TAXI TEST
 	}
 	
 	override func update(_ currentTime: TimeInterval) {
+		
+		self.highscoreLabel = self.childNode(withName: "highscore") as! SKLabelNode
+		
 		Background.shared.backgroundRoll()
 		Background.shared.backgroundOutOfScreen()
 		
+		if hasColision() {
+			luber.spriteNode.removeAction(forKey: "moveToLeft")
+			luber.spriteNode.removeAction(forKey: "moveToRight")
+			Background.shared.speed = 0
+			
+			for taxi in taxis {
+				taxi.spriteNode.removeAction(forKey: "taxiTest")
+			}
+		}
+	}
+	
+	func addTaxi(atLane lane: Int, carYDistance yCars: CGFloat, taxiSpeed: Float){
+		let taxi = Taxi(spriteName: TAXI_SPRITE_NAME, currentLane: lane, speed: taxiSpeed)
+		let taxiHeight = taxi.spriteNode.frame.size.height
 		
-//		if(Background.shared.distance > Float(endGame)){
-//			endGameState()
-//			
-//		
-//		
-//		}
+		var laneX = lane1.frame.origin.x
+		if(lane == 2){
+			laneX = lane2.frame.origin.x
+		}else if(lane == 3){
+			laneX = lane3.frame.origin.x
+		}
 		
+		taxi.spriteNode.position = CGPoint(x: laneX, y: self.frame.size.height/2+taxiHeight/2+taxiHeight*yCars)
+		self.taxis.append(taxi)
+		self.addChild(taxi.spriteNode)
+	}
+	
+	func hasColision() -> Bool {
+		let x = luber.spriteNode.position.x
+		let y = luber.spriteNode.position.y
+		
+		for taxi in taxis {
+			if x > (taxi.spriteNode.position.x - taxi.spriteNode.size.width) && x < (taxi.spriteNode.position.x + taxi.spriteNode.size.width) {
+				if y > (taxi.spriteNode.position.y - taxi.spriteNode.size.height) && y < (taxi.spriteNode.position.y + taxi.spriteNode.size.height) {
+					print("\nCOLIDIU\n")
+					return true
+				}
+			}
+		}
+		
+		return false
 	}
 	
 	func endGameState(){
@@ -90,38 +123,11 @@ class GameManagerScene: SKScene {
 				
 			}
 			
-			highscoreLabel.text = String(highscore)
+			highscoreLabel?.text = String(highscore)
 		}
-		
-		
-		
-		
-		
-	}
-	
-	func addTaxi(atLane lane: Int, carYDistance yCars: CGFloat, taxiSpeed: Float){
-		
-		var taxi = Taxi(spriteName: TAXI_SPRITE_NAME, currentLane: lane, speed: taxiSpeed)
-		
-		var taxiHeight = taxi.spriteNode.frame.size.height
-		
-		var laneX = lane1.frame.origin.x
-		
-		if(lane == 2){
-			
-			laneX = lane2.frame.origin.x
-			
-		}else if(lane == 3){
-			
-			laneX = lane3.frame.origin.x
-			
-		}
-		
-		taxi.spriteNode.position = CGPoint(x: laneX, y: self.frame.size.height/2+taxiHeight/2+taxiHeight*yCars)
-		taxi.spriteNode.color = SKColor.red
-		self.taxis.append(taxi)
-		self.addChild(taxi.spriteNode)
 		
 	}
 	
 }
+
+
