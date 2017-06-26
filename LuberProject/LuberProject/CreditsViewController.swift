f0e800e20122d4ca3d2dbd447b017cc98e1f3308
Cristiano Salla Lunardi <cristianosalla@gmail.com>
2017-06-26 16:57:54 -0300

    THE COMMIT MASTER FODA

---
+++ LuberProject/LuberProject/CreditsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreditsViewController.swift
+//  LuberProject
+//
+//  Created by Cristiano Salla Lunardi on 6/26/17.
+//  Copyright © 2017 Victor S Melo. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+	
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
+
+}
